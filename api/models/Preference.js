d9a1a5d482f5db2bda2b23710bfe0c63211bb70b
Carrington <daniel@danielingraham.com>
2013-10-06 22:35:47 +0000

    preference model

---
+++ api/models/Preference.js
@@ -0,0 +1,24 @@
+/**
+ * Preference
+ *
+ * @module      :: Cyclopedia (Sails)
+ * @description :: Preference storage for user to specify what content they want to see/what content they want to have hidden.
+ * @todo        :: When Waterline is updated with associations, this User has a one-to-many relationship with this model.
+ */
+
+module.exports = {
+
+  attributes: {
+    user: {
+      type='integer',
+      require=true
+    },
+    exclusionValues: {
+      type='array'
+    },
+    inclusionValues: {
+      type='array'
+    }    
+  }
+
+};
